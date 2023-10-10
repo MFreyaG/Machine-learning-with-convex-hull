b9f3b9114e28418ec05b5c2c0e810345bc80571e
MFreyaG <matheusgrandinetti@gmail.com>
2023-10-10 15:59:24 -0300

    main.py is now clean!

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from entities.point import Point
-from utils.convex_hull import ConvexHull
-
-n = int(input())
-point_set = [Point(input(),input()) for x in range(0,n)]
-
-for point in point_set:
-    point.print_coordinates()
-    
-hull = ConvexHull(point_set)
+# Main is now clean!
