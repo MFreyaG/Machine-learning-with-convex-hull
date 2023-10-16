7c11c5fe4e07a1ff634c192b583b2fd3b23a0704
MFreyaG <matheusgrandinetti@gmail.com>
2023-10-15 21:19:22 -0300

    Convex hull working :)

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from entities.point import Point
 from utils.convex_hull import ConvexHull
 
 n = int(input())
-point_set = [Point(input(),input()) for x in range(0,n)]
+point_set = [Point(float(input()),float(input())) for x in range(0,n)]
 
-for point in point_set:
-    point.print_coordinates()
-    
 hull = ConvexHull(point_set)
+
+hull.find_convex_hull()
+hull.print_convex_hull()
--- utils/convex_hull.py
+++ utils/convex_hull.py
@@ -6,17 +6,29 @@ class ConvexHull:
         self.point_set = point_set
         self.hull = []
         self.tools = Tools
+    
+    def print_point_set(self):
+        for point in self.point_set:
+            point.print_coordinates()
+        
+    def find_point_index(self, p):
+        index = 0
+        
+        for point in self.point_set:
+            if p.x == point.x and p.y == point.y:
+                return index
+            index += 1
         
     def find_ancor(self):
         ancor = self.point_set[0]
         
-        for point in s:
+        for point in self.point_set:
             if point.x < ancor.x:
                 ancor = point
         
         return ancor
     
-    def find_next_point(self, p, q):
+    def find_next_ancor(self, p, q):
         for point in self.point_set:
             if self.tools.find_orientation(p,q,point) == 1:
                 q = point
@@ -24,19 +36,23 @@ class ConvexHull:
         return q
     
     def find_convex_hull(self):
-        current_point = self.find_ancor()
-        next_point = self.point_set[1]
+        set_size = len(self.point_set)
         
-        self.hull.append(current_point)
+        ancor = self.find_ancor()
+        self.hull.append(ancor)
+        next = self.point_set[(self.find_point_index(ancor)+1)%set_size]
         
-        while next_point.x != self.hull[0].x and next_point.y != self.hull[0].y:
-            current_point = self.find_next_point(current_point, next_point)
-            next_point = self.point_set[2]
+        current = self.find_next_ancor(ancor, next)
+        
+        while True:
+            self.hull.append(current)
+            next = self.point_set[(self.find_point_index(current)+1)%set_size]
+            current = self.find_next_ancor(current, next)
             
-            self.hull.append(current_point)
-            
+            if current.x == self.hull[0].x and current.y == self.hull[0].y:
+                break
         
-    def print_convext_hull(self):
+    def print_convex_hull(self):
         for point in self.hull:
             point.print_coordinates()
             
