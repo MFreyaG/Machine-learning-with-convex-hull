7d9e97003916c86277d12c3455f91765c456ec76
MFreyaG <matheusgrandinetti@gmail.com>
2023-10-15 21:25:51 -0300

    test added inside main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from entities.point import Point
 from utils.convex_hull import ConvexHull
 
-n = int(input())
-point_set = [Point(float(input()),float(input())) for x in range(0,n)]
+point_set = []
+
+point_set.append(Point(0,3))
+point_set.append(Point(0,0)) 
+point_set.append(Point(1,1))
+point_set.append(Point(2,2))
+point_set.append(Point(3,3))
+point_set.append(Point(4,4))
+point_set.append(Point(3,1))
+point_set.append(Point(1,2))
 
 hull = ConvexHull(point_set)
-
 hull.find_convex_hull()
 hull.print_convex_hull()
